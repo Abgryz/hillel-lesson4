8317598908a982576c755929c4011e25c686eb50
Abgryz <v.anufriev2003@gmail.com>
2023-04-23 17:04:24 +0300

    done

---
+++ src/main/java/ithillel/lesson21/Hero.java
@@ -0,0 +1,21 @@
+package ithillel.lesson21;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Hero {
+    private Long id;
+    private String name;
+    private String gender;
+    private String eyeColor;
+    private String race;
+    private String hairColor;
+    private double height;
+    private Long publisherId;
+    private String skinColor;
+    private String alignment;
+    private int weight;
+}
+
