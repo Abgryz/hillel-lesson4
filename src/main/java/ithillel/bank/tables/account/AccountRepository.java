df8bbcd422d42b7fb5f608fdc39af93ef15ba320
Abgryz <106313916+Abgryz@users.noreply.github.com>
2023-06-15 12:47:31 +0300

    Lesson30 (#26)
    * fix
    
    * integration tests
    
    * fix
    
    * fix

--- src/main/java/ithillel/bank/tables/account/AccountRepository.java
+++ src/main/java/ithillel/bank/tables/account/AccountRepository.java
@@ -1,6 +1,7 @@
 package ithillel.bank.tables.account;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -8,6 +9,12 @@ import java.util.Optional;
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
     Optional<Account> findAccountByUid(String uid);
-
     void deleteAccountByUid(String uid);
+
+    @Query(value = """
+        select * from account
+        order by id desc
+        limit 1
+    """, nativeQuery = true)
+    Optional<Account> findLatest();
 }
